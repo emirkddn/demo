28e16404aa83846c0e82d4e5e6dc6f7acd345f03
Emir Kaan ERTUĞRUL <118385373+emirkddn@users.noreply.github.com>
2025-03-06 22:49:10 +0300

    player iyileştrmesi
    player hareketi

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+
+    private void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
         
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        else if (moveDelta.x <0)
+        {
+            transform.localScale = new Vector3(-1, -1, 1);
+        }
+
+        transform.Translate(moveDelta * Time.deltaTime);
     }
 }
